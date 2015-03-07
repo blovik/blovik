2b7613a45c79dd0bcc26ff20be7ad5e90e7b6305
blovik <blovikbiz@gmail.com>
2015-03-07 14:56:34 -0800

    updating

---
+++ Final/js/script.js
@@ -0,0 +1,5 @@
+function description() {
+    var str1 = document.getElementById("disc1").innerHTML; 
+    var res = str.replace(/blue/g, "red");
+    document.getElementById("disc1").innerHTML = res;
+}
