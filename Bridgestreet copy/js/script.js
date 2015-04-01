00b4e5dbe3ca7bedb4308a5c64ce007286a9056d
brian <blovikbiz@gmail.com>
2015-03-31 18:59:52 -0700

    emptied repository

--- Bridgestreet copy/js/script.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-	alert("Bridge Street Bargains:\n\n(559) 732 6149\n\ndarin@gmail.com\n\n715 S. Bridge St.\nVisalia, CA 93277\n\nStore hours (Pacific time):\n8:00 - 5:00 Tuesday - Friday\n8:00 - 3:00 Saturday\nClosed Sunday & Monday");
-}
-function goBack() {
-    window.history.back()
-}
